dff734c9f5b47bda83949880fcea73b47834acfa
James Preiss <japreiss@usc.edu>
2017-11-14 23:18:48 -0800

    add tolerance argument to pp_is_smooth

--- pp_is_smooth.m
+++ pp_is_smooth.m
@@ -1,7 +1,10 @@
 % check if the piecewise polynomial is smooth.
 % i.e. if order==2 checks up thru the 2nd derivative.
 % returns boolean.
-function ok = pp_is_smooth(pp, order)
+function ok = pp_is_smooth(pp, order, tolerance)
+	if nargin < 3
+		tolerance = 0.0000001;
+	end
 	EPS = 0.0000000001;
 	for i=0:order
 		splices = pp.breaks(2:(end-1));
@@ -10,7 +13,7 @@ function ok = pp_is_smooth(pp, order)
 			t1 = t+EPS;
 			x0 = ppval(pp, t0);
 			x1 = ppval(pp, t1);
-			if ~close(x0, x1)
+			if ~close(x0, x1, tolerance)
 				ok = false;
 				return;
 			end
@@ -20,9 +23,9 @@ function ok = pp_is_smooth(pp, order)
 	ok = true;
 end
 
-function c = close(a, b)
+function c = close(a, b, tolerance)
 	relative_err = abs(a(:) - b(:));
 	nz = a ~= 0;
 	relative_err(nz) = relative_err(nz) ./ abs(a(nz));
-	c = all(relative_err < 0.0000001);
+	c = all(relative_err < tolerance);
 end
